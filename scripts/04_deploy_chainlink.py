abff18934445842772e7abe87b7f80b9d6351ea8
iHeroN <iherondev@gmail.com>
2022-08-19 10:55:25 +0200

    Adding deployment scripts

--- scripts/04_deploy_chainlink.py
+++ scripts/04_deploy_chainlink.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 from brownie import (
-    KeeperProxyCoreStrategy,
+    ChainlinkUpkeep,
     TransparentUpgradeableProxy,
     ProxyAdmin,
     config,
@@ -15,18 +15,17 @@ from scripts.helpful_scripts import get_account, encode_function_data
 def main():
     dev = accounts.load(click.prompt("Account", type=click.Choice(accounts.load())))
     print(f"Deploying to {network.show_active()}")
-    implementation = KeeperProxyCoreStrategy.deploy(
+    implementation = ChainlinkUpkeep.deploy(
         {"from": dev},
         publish_source=True
     )
     # Optional, deploy the ProxyAdmin and use that as the admin contract
-    proxy_admin_address = "0xBE9A3A675D20278F2327ECEF1D574bfF676F1262"
-
     # If we want an intializer function we can add
     # `initializer=box.store, 1`
     # to simulate the initializer being the `store` function
     # with a `newValue` of 1
-    box_encoded_initializer_function = implementation.initialize.encode_input("0x0795eCB44B3e30C88C3BD1Af93c76A45FCba2508",10,10)
+    box_encoded_initializer_function = implementation.initialize.encode_input(dev.address,"0x409CF388DaB66275dA3e44005D182c12EeAa12A0")
+    proxy_admin_address = "0xBE9A3A675D20278F2327ECEF1D574bfF676F1262"
 
     # _strategy, uint256 _hysteriaDebt, uint256 _hysteriaCollateral
 
